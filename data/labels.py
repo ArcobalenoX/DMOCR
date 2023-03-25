c11960498342c752a14d7aedb5af19953525eead
arcobaleno <deityxsky@foxmail.com>
2023-03-25 18:31:55 +0800

    update labels

--- data/labels.py
+++ data/labels.py
@@ -26,7 +26,7 @@ def json2txt_CRNN(path_json,path_txt):
         with open(path_txt,'w+') as ftxt:
             for shape in jsonx['shapes']:           
                 label=str(shape['label'])  
-                strxy = os.path.split(path_txt)[1].replace('txt','jpg')+" "+label
+                strxy = path_txt.replace('.txt','.jpg')[-7:]+" "+label
                 print(strxy)                                             
                 ftxt.writelines(strxy+"\n")  
 
@@ -49,9 +49,12 @@ def crnn_label(dir_json,dir_txt):
         json2txt_CRNN(path_json,path_txt)
 
 if __name__ == "__main__":
-    root_path = r'E:\Code\Python\datas\meter\DMkinds'
-    dir_json = os.path.join(root_path,r'DM1json')
-    dir_east_txt = os.path.join(root_path,r'DM1east')
-    dir_crnn_txt = os.path.join(root_path,r'DM1crnn')
+    dir_json = r'E:\Code\Python\datas\meter\DMkinds\DM4json'
+    dir_east_txt = r'E:\Code\Python\datas\meter\DMkinds\DM4east'
+    dir_crnn_txt = r'E:\Code\Python\datas\meter\DMkinds\DM4crnn'
+    if not (os.path.exists(dir_east_txt)):
+        os.mkdir(dir_east_txt)
+    if not (os.path.exists(dir_crnn_txt)):
+        os.mkdir(dir_crnn_txt)    
     east_label(dir_json,dir_east_txt)
     crnn_label(dir_json,dir_crnn_txt)
